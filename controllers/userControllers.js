9239a08eef4ea05b938eb905997d3d514c8cfb31
HectorJosueDH <hectorjosue78@gmail.com>
2023-07-17 16:26:50 -0600

    User routes and functionality complete

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -15,7 +15,6 @@ module.exports = {
   // get one user by generated id
   async getUserById(req, res) {
     try {
-      // Used req.params.userId because in userRoutes the route is /:userId
       const userData = await User.findById(req.params.userId);
       if (!userData) {
         return res.status(404).json({ message: "No user with that ID" });
@@ -28,9 +27,9 @@ module.exports = {
   },
 
   // createUser
-  async createUser({ body }, res) {
+  async createUser(req, res) {
     try {
-      const userData = await User.create(body);
+      const userData = await User.create(req.body);
       res.json(userData);
     } catch (err) {
       console.log(err);
@@ -39,11 +38,13 @@ module.exports = {
   },
 
   // updateUser
-  async updateUser({ params, body }, res) {
+  async updateUser(req, res) {
     try {
-      const userData = await User.findOneAndUpdate({ _id: params.id }, body, {
-        new: true,
-      });
+      const userData = await User.findOneAndUpdate(
+        { _id: req.params.userId },
+        req.body,
+        { new: true }
+      );
       res.json(userData);
     } catch (err) {
       console.log(err);
@@ -52,9 +53,9 @@ module.exports = {
   },
 
   // deleteUser
-  async deleteUser({ params }, res) {
+  async deleteUser(req, res) {
     try {
-      const userData = await User.findOneAndDelete({ _id: params.id });
+      const userData = await User.findOneAndDelete({ _id: req.params.userId });
       res.json(userData);
     } catch (err) {
       console.log(err);
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,16 +1,16 @@
 const router = require('express').Router();
 const {
-    getUsers,
-    getUserById,
-    createUser,
-    deleteUser,
-    updateUser,
+  getUsers,
+  getUserById,
+  createUser,
+  deleteUser,
+  updateUser,
 } = require('../../controllers/userControllers');
 
 // /api/users
 router.route('/').get(getUsers).post(createUser);
 
 // /api/users/:userId
-router.route('/:userId').get(getUserById).delete(deleteUser);
+router.route('/:userId').get(getUserById).put(updateUser).delete(deleteUser);
 
 module.exports = router;
