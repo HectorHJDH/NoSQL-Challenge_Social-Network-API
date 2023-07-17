81a5995625082bd7d7fb6f191102c0b445fc0f25
HectorJosueDH <hectorjosue78@gmail.com>
2023-07-17 12:06:50 -0600

    Moved the Reaction model to his own file / created userControllers.js

--- models/Thought.js
+++ models/Thought.js
@@ -1,27 +1,5 @@
 const { Schema, model } = require("mongoose");
-
-const reactionSchema = new Schema({
-  reactionId: {
-    type: Schema.Types.ObjectId,
-    default: () => new mongoose.Types.ObjectId(),
-  },
-  reactionBody: {
-    type: String,
-    required: true,
-    maxlength: 280,
-  },
-  username: {
-    type: String,
-    required: true,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-    get: function (timestamp) {
-      return new Date(timestamp).toISOString();
-    },
-  },
-});
+const reactionSchema = require('./Reaction');
 
 const thoughtSchema = new Schema(
   {
